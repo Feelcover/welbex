f406093aafa7d43a5828c2005dd8bde985e76885
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-11-21 19:47:44 +0300

    адаптировать хедер и фон Layout под мобильную версию

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,35 +1,38 @@
+import { motion } from "framer-motion";
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import { motion } from "framer-motion";
 import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className={styles.layout}>
     <Header />
+    <div className={styles.layout__mobilebg} />
     <div className={styles.layout__redlight} />
     <div className={styles.layout__purplelight}>
       <motion.div
         transition={{ delay: 0.1 }}
-        initial={{ opacity:0 }}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
         <div className={styles.layout__purplelight__purpleball} />
       </motion.div>
       <motion.div
         transition={{ delay: 0.2 }}
-        initial={{ opacity:0 }}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
         <div className={styles.layout__purplelight__redball} />
       </motion.div>
       <motion.div
         transition={{ delay: 0.3 }}
-        initial={{ opacity:0 }}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <div className={styles.layout__purplelight__redballsmall} /></motion.div>
+        <div className={styles.layout__purplelight__redballsmall} />
+      </motion.div>
     </div>
+    {children}
     <Footer />
   </div>
 );
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,12 +3,17 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import { motion } from "framer-motion";
 import styles from "./Header.module.scss";
+import { useMediaQuery } from "@/hooks";
 
 const Header = () => {
+  const isTablet = useMediaQuery(1150);
+  const isMobile = useMediaQuery(550);
+
+
   return (
     <header className={styles.header}>
       <div className={styles.leftContainer}>
-        <Logo />
+        {!isTablet && <Logo />}
         <nav className={styles.leftContainer__menu}>
           <motion.div
             className={styles.leftContainer__menu__item}
@@ -62,82 +67,77 @@ const Header = () => {
           >
             <Link href="#">Кейсы</Link>
           </motion.div>
+          {!isMobile && (
+            <motion.div
+              className={styles.leftContainer__menu__item}
+              transition={{
+                delay: 0.5,
+                ease: "linear",
+                duration: 0.5,
+                x: { duration: 1 },
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <Link href="#">Сертификаты</Link>
+            </motion.div>
+          )}
+        </nav>
+      </div>
+      {!isTablet && (
+        <div className={styles.rightContainer}>
           <motion.div
-            className={styles.leftContainer__menu__item}
             transition={{
-              delay: 0.5,
+              delay: 0.6,
               ease: "linear",
-              duration: 0.5,
+              duration: 0.6,
               x: { duration: 1 },
             }}
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
           >
-            <Link href="#">Сертификаты</Link>
+            <p className={styles.rightContainer__number}>+7 555 555-55-55</p>
           </motion.div>
-        </nav>
-      </div>
-      <div className={styles.rightContainer}>
-        <motion.div
-          transition={{
-            delay: 0.6,
-            ease: "linear",
-            duration: 0.6,
-            x: { duration: 1 },
-          }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-        >
-          <p className={styles.rightContainer__number}>+7 555 555-55-55</p>
-        </motion.div>
-        <nav className={styles.rightContainer__nav}>
-          <motion.div
-            className={`${styles.rightContainer__nav__item} ${styles.tg}`}
-            transition={{
-              delay: 0.7,
-              ease: "linear",
-              duration: 0.6,
-            }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-            <Link
-              href="#"
-              
-            />
-          </motion.div>
-          <motion.div
-            className={`${styles.rightContainer__nav__item} ${styles.vb}`}
-            transition={{
-              delay: 0.8,
-              ease: "linear",
-              duration: 0.6,
-            }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-            <Link
-              href="#"
-              
-            />
-          </motion.div>
-          <motion.div
-            className={`${styles.rightContainer__nav__item} ${styles.ws}`}
-            transition={{
-              delay: 0.9,
-              ease: "linear",
-              duration: 0.6,
-            }}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-          >
-            <Link
-              href="#"
-              
-            />
-          </motion.div>
-        </nav>
-      </div>
+          <nav className={styles.rightContainer__nav}>
+            <motion.div
+              className={`${styles.rightContainer__nav__item} ${styles.tg}`}
+              transition={{
+                delay: 0.7,
+                ease: "linear",
+                duration: 0.6,
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <Link href="#" />
+            </motion.div>
+            <motion.div
+              className={`${styles.rightContainer__nav__item} ${styles.vb}`}
+              transition={{
+                delay: 0.8,
+                ease: "linear",
+                duration: 0.6,
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <Link href="#" />
+            </motion.div>
+            <motion.div
+              className={`${styles.rightContainer__nav__item} ${styles.ws}`}
+              transition={{
+                delay: 0.9,
+                ease: "linear",
+                duration: 0.6,
+              }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+            >
+              <Link href="#" />
+            </motion.div>
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
