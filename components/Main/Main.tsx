ab10abcfc78af4cb9c6902f23d751d5e4ecf630c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-11-21 22:36:08 +0300

    часть вёрстки Main

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -15,34 +15,33 @@ const Main = () => {
 
         <section className={styles.giftsContainer}>
           <p className={styles.giftsTitle}>
-            Вместе с
-            <span className={styles.giftsTitle__Logo}>
-              Бесплатной консультацией
-            </span>
-            мы дарим:
+            Вместе с {""}
+            <span className={styles.giftsTitle__Logo}>Бесплатной{""}</span>{" "}
+            <span className={styles.giftsTitle__Logo}>консультацией</span>
+            {""} мы дарим:
           </p>
           <div className={styles.gifts}>
             <div className={styles.gifts__widgets}>
-              <p className={styles.gifts__widgets__title}>Виджеты</p>
-              <span className={styles.gifts__widgets__title}>
+              <p className={styles.gifts__title}>Виджеты</p>
+              <span className={styles.gifts__description}>
                 30 готовых решений
               </span>
             </div>
             <div className={styles.gifts__dashboard}>
-              <p className={styles.gifts__widgets__title}>Dashboard</p>
-              <span className={styles.gifts__widgets__title}>
+              <p className={styles.gifts__title}>Dashboard</p>
+              <span className={styles.gifts__description}>
                 с показателями вашего бизнеса
               </span>
             </div>
             <div className={styles.gifts__skype}>
-              <p className={styles.gifts__widgets__title}>Skype Аудит</p>
-              <span className={styles.gifts__widgets__title}>
+              <p className={styles.gifts__title}>Skype Аудит</p>
+              <span className={styles.gifts__description}>
                 отдела продаж и CRM системы
               </span>
             </div>
             <div className={styles.gifts__crm}>
-              <p className={styles.gifts__widgets__title}>35 дней</p>
-              <span className={styles.gifts__widgets__title}>
+              <p className={styles.gifts__title}>35 дней</p>
+              <span className={styles.gifts__description}>
                 использования CRM
               </span>
             </div>
