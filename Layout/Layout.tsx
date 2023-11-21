8209414ec6fe3c4f250bb793e0d8a6db7fbf86ec
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-11-21 15:26:47 +0300

    добавить объемные шары в Layout

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import styles from "./Layout.module.scss"
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
-  <>
+  <div className={styles.layout}>
     <Header />
+    <div className={styles.layout__redlight}/>
+    <div className={styles.layout__purplelight}/>
     <Footer />
-  </>
+  </div>
 );
 
 export default Layout;
