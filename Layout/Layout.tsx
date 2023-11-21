d2dfe110ef6b070af677baa8b131d1207289f452
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-11-21 18:55:47 +0300

    сверстать фон Layout для десктопа, сверстать header для декстопа, реализовать хук useMediaQuery

--- Layout/Layout.tsx
+++ Layout/Layout.tsx
@@ -1,13 +1,35 @@
 import React from "react";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
-import styles from "./Layout.module.scss"
+import { motion } from "framer-motion";
+import styles from "./Layout.module.scss";
 
 const Layout = ({ children }: { children: React.ReactNode }) => (
   <div className={styles.layout}>
     <Header />
-    <div className={styles.layout__redlight}/>
-    <div className={styles.layout__purplelight}/>
+    <div className={styles.layout__redlight} />
+    <div className={styles.layout__purplelight}>
+      <motion.div
+        transition={{ delay: 0.1 }}
+        initial={{ opacity:0 }}
+        animate={{ opacity: 1 }}
+      >
+        <div className={styles.layout__purplelight__purpleball} />
+      </motion.div>
+      <motion.div
+        transition={{ delay: 0.2 }}
+        initial={{ opacity:0 }}
+        animate={{ opacity: 1 }}
+      >
+        <div className={styles.layout__purplelight__redball} />
+      </motion.div>
+      <motion.div
+        transition={{ delay: 0.3 }}
+        initial={{ opacity:0 }}
+        animate={{ opacity: 1 }}
+      >
+        <div className={styles.layout__purplelight__redballsmall} /></motion.div>
+    </div>
     <Footer />
   </div>
 );
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,14 +1,143 @@
+import Link from "next/link";
 import React from "react";
 import Logo from "../Logo/Logo";
+import { motion } from "framer-motion";
 import styles from "./Header.module.scss";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.leftContainer}>
-        <Logo/>
+        <Logo />
+        <nav className={styles.leftContainer__menu}>
+          <motion.div
+            className={styles.leftContainer__menu__item}
+            transition={{
+              delay: 0.5,
+              ease: "linear",
+              duration: 0.5,
+              x: { duration: 1 },
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link href="#">Услуги</Link>
+          </motion.div>
+          <motion.div
+            className={styles.leftContainer__menu__item}
+            transition={{
+              delay: 0.4,
+              ease: "linear",
+              duration: 0.5,
+              x: { duration: 1 },
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link href="#">Виджеты</Link>
+          </motion.div>
+          <motion.div
+            className={styles.leftContainer__menu__item}
+            transition={{
+              delay: 0.3,
+              ease: "linear",
+              duration: 0.5,
+              x: { duration: 1 },
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link href="#">Интеграции</Link>
+          </motion.div>
+          <motion.div
+            className={styles.leftContainer__menu__item}
+            transition={{
+              delay: 0.4,
+              ease: "linear",
+              duration: 0.5,
+              x: { duration: 1 },
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link href="#">Кейсы</Link>
+          </motion.div>
+          <motion.div
+            className={styles.leftContainer__menu__item}
+            transition={{
+              delay: 0.5,
+              ease: "linear",
+              duration: 0.5,
+              x: { duration: 1 },
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link href="#">Сертификаты</Link>
+          </motion.div>
+        </nav>
       </div>
-      <div className={styles.rightContainer}></div>
+      <div className={styles.rightContainer}>
+        <motion.div
+          transition={{
+            delay: 0.6,
+            ease: "linear",
+            duration: 0.6,
+            x: { duration: 1 },
+          }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <p className={styles.rightContainer__number}>+7 555 555-55-55</p>
+        </motion.div>
+        <nav className={styles.rightContainer__nav}>
+          <motion.div
+            className={`${styles.rightContainer__nav__item} ${styles.tg}`}
+            transition={{
+              delay: 0.7,
+              ease: "linear",
+              duration: 0.6,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link
+              href="#"
+              
+            />
+          </motion.div>
+          <motion.div
+            className={`${styles.rightContainer__nav__item} ${styles.vb}`}
+            transition={{
+              delay: 0.8,
+              ease: "linear",
+              duration: 0.6,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link
+              href="#"
+              
+            />
+          </motion.div>
+          <motion.div
+            className={`${styles.rightContainer__nav__item} ${styles.ws}`}
+            transition={{
+              delay: 0.9,
+              ease: "linear",
+              duration: 0.6,
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+          >
+            <Link
+              href="#"
+              
+            />
+          </motion.div>
+        </nav>
+      </div>
     </header>
   );
 };
